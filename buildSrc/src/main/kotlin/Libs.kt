01839b7277c60d0f812f96f4224ba4f0a138562c
Eric Roberto <58601124+ericDevRoberto@users.noreply.github.com>
2022-03-23 08:37:54 -0300

    gradle added

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,10 @@
+object Libs {
+    const val androidCore = "androidx.core:core-ktx:${Version.androidCore}"
+    const val appCompat = "androidx.appcompat:appcompat:${Version.appCompat}"
+    const val material = "com.google.android.material:material:${Version.material}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Version.constraintLayout}"
+    const val lifecycleLiveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Version.lifecycle}"
+    const val lifeCycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.lifecycle}"
+    const val navgationFragment = "androidx.navigation:navigation-fragment-ktx:${Version.navgation}"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:${Version.navgation}"
+}
---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,9 @@
+object Version {
+
+    const val androidCore = "1.7.0"
+    const val appCompat = "1.4.1"
+    const val material = "1.5.0"
+    const val constraintLayout = "2.1.3"
+    const val lifecycle = "2.4.1"
+    const val navgation = "2.4.1"
+}
